3657d1b61ef03cdf26bc3bd2492ca30e759f993c
Marc Colburn <colburnmarc@gmail.com>
2022-06-26 16:53:14 -0400

    Added rack types and started templates.

--- aos_api_main.py
+++ aos_api_main.py
@@ -33,7 +33,7 @@ def aos_login(api_base_url, aos_user, aos_pass):
 def aos_put(api_url, headers, data):
     '''PUT against API resource to create new item'''
     response = request("POST", api_url, data=data, headers=headers, verify=False)
-    if response.status_code != 202:
+    if response.status_code != 202 and response.status_code != 201:
         sys.exit('error: data post failed')
     return response
 
@@ -56,7 +56,7 @@ def aos_delete(api_url, headers, delete_pending_data):
             if item['display_name'] == data['name']:
                 delete_url = '{0}/{1}'.format(api_url, item['id'])
                 data_response = request("DELETE", delete_url, headers=headers, verify=False)
-                if data_response.status_code != 202:
+                if data_response.status_code != 202 and data_response.status_code != 204:
                     sys.exit('error deleting element')
 
 
@@ -69,9 +69,10 @@ def aos_create_data(var_data, template_name):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--create", help="create elements passed, asn_pools, ip_pools")
+    parser.add_argument("--create", help="create elements passed, asn_pools, ip_pools, vni_pools")
     parser.add_argument("--delete", help="delete elements passed, asn_pools, ip_pools")
     parser.add_argument("--get", help="get elements passed, asn_pools, ip_pools")
+    parser.add_argument("--type", help="type of elements to pass, resources, design, etc.")
     args = parser.parse_args()
     aos_server = '10.1.94.10'
     aos_user = 'admin'
@@ -88,30 +89,39 @@ if __name__ == '__main__':
             autoescape=select_autoescape()
     )
     if args.create:
-        pool_api_url = api_base_url + 'resources/{0}'.format(args.create.replace("_", "-"))
+        if args.type == 'resources':
+            api_url = api_base_url + 'resources/{0}'.format(args.create.replace("_", "-"))
+        elif args.type == 'design':
+            api_url = api_base_url + 'design/{0}'.format(args.create.replace("_", "-"))
         if args.create == 'asn_pools':
             for asn_range in yaml_data['asn_ranges']:
                 template_name = "create_asn_pools.j2"
                 asn_create_data = aos_create_data(asn_range, template_name)
-                asn_response = aos_put(asn_api_url, head_w_token, asn_create_data)
+                asn_response = aos_put(api_url, head_w_token, asn_create_data)
         else:
             for pool in yaml_data[args.create]:
                 template_name = "create_{0}.j2".format(args.create)
                 create_data = aos_create_data(pool, template_name)
-                response = aos_put(pool_api_url, head_w_token, create_data)
+                response = aos_put(api_url, head_w_token, create_data)
 
     if args.delete:
-        pool_api_url = api_base_url + 'resources/{0}'.format(args.delete.replace("_", "-"))
+        if args.type == 'resources':
+            api_url = api_base_url + 'resources/{0}'.format(args.delete.replace("_", "-"))
+        elif args.type == 'design':
+            api_url = api_base_url + 'design/{0}'.format(args.delete.replace("_", "-"))
         if args.delete == 'asn_pools':
-            aos_delete(asn_api_url, head_w_token, yaml_data['asn_ranges'])
+            aos_delete(api_url, head_w_token, yaml_data['asn_ranges'])
         else:
-            aos_delete(pool_api_url, head_w_token, yaml_data[args.delete])
+            aos_delete(api_url, head_w_token, yaml_data[args.delete])
 
     if args.get:
-        pool_api_url = api_base_url + 'resources/{0}'.format(args.get.replace("_", "-"))
+        if args.type == 'resources':
+            api_url = api_base_url + 'resources/{0}'.format(args.get.replace("_", "-"))
+        elif args.type == 'design':
+            api_url = api_base_url + 'design/{0}'.format(args.get.replace("_", "-"))
         if args.get == 'asn_pools':
-            asn_ranges = aos_get(asn_api_url, head_w_token)
+            asn_ranges = aos_get(api_url, head_w_token)
             pprint(asn_ranges.json())
         else:
-            var_data = aos_get(pool_api_url, head_w_token)
+            var_data = aos_get(api_url, head_w_token)
             pprint(var_data.json())
