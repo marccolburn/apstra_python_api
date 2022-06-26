5b7c659e4f509396ef1299a263e7c93a3990b09d
Marc Colburn <colburnmarc@gmail.com>
2022-06-25 22:04:33 -0400

    Added argparse and used arguments to invoke create/delete/get functions

--- aos_api_main.py
+++ aos_api_main.py
@@ -5,7 +5,9 @@ from pprint import pprint
 import requests
 import sys
 import yaml
+import json
 import pdb
+import argparse
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -41,6 +43,22 @@ def aos_get(api_url, headers):
         sys.exit('error')
     return response
 
+def aos_delete(api_url, headers, delete_pending_data):
+    '''Pass data to be deleted'''
+    data_response = request("GET", api_url, headers=headers, verify=False)
+    if data_response.status_code != 200:
+        sys.exit('error')
+    
+    json_data = data_response.json()
+    for item in json_data['items']:
+        for data in delete_pending_data:
+            if item['display_name'] == data['name']:
+                delete_url = '{0}/{1}'.format(api_url, item['id'])
+                data_response = request("DELETE", delete_url, headers=headers, verify=False)
+                if data_response.status_code != 202:
+                    sys.exit('error deleting element')
+
+
 def aos_asn_pool_create_data(api_base_url, asn_range):
     '''Provides data structure and api path for creating ASN Pools'''
     data = '''
@@ -60,6 +78,11 @@ def aos_asn_pool_create_data(api_base_url, asn_range):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create", help="create elements passed, asn_pools")
+    parser.add_argument("--delete", help="delete elements passed, asn_pools")
+    parser.add_argument("--get", help="get elements passed, asn_pools")
+    args = parser.parse_args()
     aos_server = '10.1.94.10'
     aos_user = 'admin'
     aos_pass = getpass('password: ')
@@ -68,11 +91,15 @@ if __name__ == '__main__':
     yaml_file = open('apstra_vars.yaml', 'r')
     yaml_data = yaml.load(yaml_file, Loader=yaml.Loader)
     yaml_file.close()
-    for asn_range in yaml_data['asn_ranges']:
-        asn_create_data, asn_create_url = aos_asn_pool_create_data(api_base_url, asn_range)
-        asn_response = aos_put(asn_create_url, head_w_token, asn_create_data)
-        print(asn_response.status_code)
-
     asn_api_url = api_base_url + 'resources/asn-pools'
-    asn_ranges = aos_get(asn_api_url, head_w_token)
-    pprint(asn_ranges.json())
+    if args.create and args.create == 'asn_pools':
+        for asn_range in yaml_data['asn_ranges']:
+            asn_create_data, asn_create_url = aos_asn_pool_create_data(api_base_url, asn_range)
+            asn_response = aos_put(asn_create_url, head_w_token, asn_create_data)
+
+    if args.delete and args.delete == 'asn_pools':
+        aos_delete(asn_api_url, head_w_token, yaml_data['asn_ranges'])
+
+    if args.get and args.get == 'asn_pools':
+        asn_ranges = aos_get(asn_api_url, head_w_token)
+        pprint(asn_ranges.json())
